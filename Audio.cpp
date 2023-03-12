031669dd6541f6e50720fcb99e1494dcd991c280
PauMenaTorres <paumt128@gmail.com>
2023-03-12 18:21:04 +0100

    audio

--- Audio.cpp
+++ Audio.cpp
@@ -12,7 +12,21 @@ void playMusic()
     SDL_AudioDeviceID deviceId = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
     SDL_QueueAudio(deviceId, wavBuffer, wavLength);
     SDL_PauseAudioDevice(deviceId, 0);
-    SDL_Delay(300600); // Espera 5 segundos para que se reproduzca el sonido
+
+    bool quit = false; // Variable para indicar que el usuario quiere salir
+    while (!quit) {
+        // Realiza las tareas que quieras mientras la música sigue sonando
+        // Puedes utilizar eventos de SDL para detectar cuando el usuario quiere salir
+        // Por ejemplo, puedes utilizar SDL_PollEvent para revisar si se ha detectado un evento de salida
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                quit = true; // Usuario quiere salir
+            }
+        }
+    }
+
     SDL_CloseAudioDevice(deviceId);
     SDL_FreeWAV(wavBuffer);
+
 }
